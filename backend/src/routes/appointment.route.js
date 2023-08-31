650616638f59177aa4828407a94103d5238b1cd0
RoberthLoorDev <dayvidlife@gmail.com>
2023-08-31 13:38:39 -0500

    feat(consul appointment): modification in the query route of an appointment by user id

--- backend/src/routes/appointment.route.js
+++ backend/src/routes/appointment.route.js
@@ -5,7 +5,7 @@ const router = express.Router();
 router.post("/create", AppointmetController.createAppointment);
 router.get('/consult/:identification', AppointmetController.consultAppointmentsForIdentification)
 router.get('/delete/:id', AppointmetController.deteleAppointmentForId)
-router.post('/check/appointment', AppointmetController.checkAppointmentAvailability)
+router.post('/check/', AppointmetController.checkAppointmentAvailability)
 router.get('/:id', AppointmetController.getAppointmentForId)
 
 module.exports = router;
