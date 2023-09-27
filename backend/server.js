ab7fd9f8880d44551ae5d625c33d99c6b1c630e2
RoberthLoorDev <dayvidlife@gmail.com>
2023-09-27 08:41:02 -0500

    fix(server): cors error solution

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 
 const corsOptions = {
-    origin: "http://localhost:3000",
+    origin: "https://citas-toainga.vercel.app/",
 };
 app.use(cors(corsOptions));
 
