ec49988ce69a496455ea5deda7e4ffd8f61ed7a9
RoberthLoorDev <dayvidlife@gmail.com>
2024-02-02 06:52:38 -0500

    feat: optimize images

--- frontend/src/assets/img/images.tsx
+++ frontend/src/assets/img/images.tsx
@@ -1,14 +1,16 @@
-import image_logo from "./logo.png";
-import image_header_home from "./image-header-home.png";
-import icon_create_appointment from "./icon-create-appointment.png";
-import icon_search from "./search-icon.png";
-import arrow from "./arrow.png";
+import image_logo from "./logo.webp";
+import image_header_home from "./image-header-home.webp";
+import icon_create_appointment from "./icon-create-appointment.webp";
+import icon_search from "./search-icon.webp";
+import arrow from "./arrow.webp";
+import loading from "./loading.json";
 const images = {
     image_logo,
     image_header_home,
     icon_create_appointment,
     icon_search,
     arrow,
+    loading,
 };
 
 export default images;
